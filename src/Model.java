76db755a598e72c832f1429debe3673b9da9e346
quentinms <quentinmazarssimon@gmail.com>
2013-10-24 15:20:48 +0200

    basics of State class

--- src/Model.java
+++ src/Model.java
@@ -1,8 +1,55 @@
+import java.util.ArrayList;
+
+import logist.plan.Action;
+import logist.task.Task;
+import logist.topology.Topology.City;
 
 public class Model {
 
 }
 
-class State{
+class State {
+	City currentCity;
+	ArrayList<Task> availableTasks;
+	ArrayList<Task> pickedUpTasks;
+
+	ArrayList<Action> actionList;
+
+	int totalCost;
+	int weightCarried;
+
+	public State(City currentCity, ArrayList<Task> availableTasks) {
+		this.currentCity = currentCity;
+		this.availableTasks = new ArrayList<Task>(availableTasks);
+		this.pickedUpTasks = new ArrayList<Task>();
+
+		this.actionList = new ArrayList<Action>();
+
+		totalCost = 0;
+		weightCarried = 0;
+	}
+
+	public State(City currentCity, ArrayList<Task> availableTasks,
+			ArrayList<Task> pickedUpTasks, ArrayList<Action> actionList,
+			int totalCost, int weightCarried) {
+
+		this.currentCity = currentCity;
+		// this.availableTasks = availableTasks;
+		this.availableTasks = new ArrayList<Task>(availableTasks);
+		// this.pickedUpTasks = pickedUpTasks;
+		this.pickedUpTasks = new ArrayList<Task>(pickedUpTasks);
+		// this.actionList = actionList;
+		this.actionList = new ArrayList<Action>(actionList);
+
+		this.totalCost = totalCost;
+		this.weightCarried = weightCarried;
+
+	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		State s = (State) obj;
+		return currentCity.equals(s.currentCity) && pickedUpTasks.equals(s.pickedUpTasks) 
+				&& availableTasks.equals(s.availableTasks);
+	}
 }
